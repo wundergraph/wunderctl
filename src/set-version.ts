b60bec9436a5d2047c8ae0e5b4336e1f96af861c
Jens Neuse <jens.neuse@gmx.de>
2021-01-07 13:38:23 +0100

    update pipeline

--- src/set-version.ts
+++ src/set-version.ts
@@ -1,12 +1,13 @@
 import * as fs from "fs";
 
 const setVersion = () => {
-    const version = process.env.GITHUB_REF;
-    if (!version || version === ""){
+    const githubRef = "" + process.env.GITHUB_REF;
+    if (githubRef === ""){
         console.log(`cannot set version, env var missing: GITHUB_REF`);
         process.exit(1);
         return
     }
+    const version = githubRef.substring("refs/tags/v".length);
     console.log(`detected version: ${version}`);
     try {
         const packageJSON = JSON.parse(fs.readFileSync("package.json").toString());
